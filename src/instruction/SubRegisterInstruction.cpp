797f8f7027482e6da06e3743e4879600de8ef185
Ilya Dyakun <i.dyakun@g.nsu.ru>
2025-09-01 09:29:26 +0700

    Implement SUB register instruction

---
+++ src/instruction/SubRegisterInstruction.cpp
@@ -0,0 +1,25 @@
+#include "SubRegisterInstruction.h"
+
+#include "base/BitOperations.h"
+
+namespace chip8 {
+
+
+SubRegisterInstruction::SubRegisterInstruction(word opcode) 
+    : Instruction(opcode)
+{}  
+
+
+void SubRegisterInstruction::execute(ExecutionContext& ctx) {
+  word x = getXFrom(opcode_);
+  word y = getYFrom(opcode_);
+  if (ctx.registers[x] > ctx.registers[y]) {
+    ctx.registers[kFlagRegister] = 1;
+  } else {
+    ctx.registers[kFlagRegister] = 0;
+  }
+  ctx.registers[x] -= ctx.registers[y];
+}
+
+
+} // namespace chip8
---
+++ test/instruction/SubRegisterInstructionTest.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "base/BitOperations.h"
+#include "instruction/SubRegisterInstruction.h"
+#include "InstructionTestBase.h"
+
+using namespace std;
+using namespace chip8;
+
+
+TEST(SubRegisterInstructionTest, Sub) {
+  // Arrange
+  vector<word> opcodes = { 0x8015, 0x8125, 0x8235, 0x8345, 0x8455 };
+  vector<chip8::byte> regs = { 0x80, 0x50, 0x40, 0x40, 0x20, 0x01};
+  vector<chip8::byte> expected(opcodes.size());
+  vector<chip8::byte> actual(opcodes.size());
+  size_t n = opcodes.size();
+
+  auto mem = generateMemory();
+  auto ctx = generateContextWithRegisters(regs, *mem);
+  auto ins = generateInstructions<SubRegisterInstruction>(opcodes);
+
+  for (size_t i = 0; i < n; i += 1) {
+    expected[i] = static_cast<chip8::byte>(regs[i] - regs[i + 1]);
+  }
+
+  // Act
+  for (size_t i = 0; i < n; i += 1) {
+    ins[i]->execute(*ctx);
+    actual[i] = ctx->registers[i];
+  }
+
+  // Assert
+  for (size_t i = 0; i < n; i += 1) {
+    EXPECT_EQ(actual[i], expected[i]);
+  }
+}
+
+
+TEST(SubRegisterInstructionTest, Flag) {
+  // Arrange
+  vector<word> opcodes = { 0x8015, 0x8125, 0x8235, 0x8345, 0x8455 };
+  vector<chip8::byte> regs = { 0xF0, 0xF1, 0x01, 0x40, 0xD0, 0x60};
+  vector<chip8::byte> expected = { 0, 1, 0, 0, 1};
+  vector<chip8::byte> actual(opcodes.size());
+  size_t n = opcodes.size();
+
+  auto mem = generateMemory();
+  auto ctx = generateContextWithRegisters(regs, *mem);
+  auto ins = generateInstructions<SubRegisterInstruction>(opcodes);
+
+  // Act
+  for (size_t i = 0; i < n; i += 1) {
+    ins[i]->execute(*ctx);
+    actual[i] = ctx->registers[kFlagRegister];
+  }
+
+  // Assert
+  for (size_t i = 0; i < n; i += 1) {
+    EXPECT_EQ(actual[i], expected[i]);
+  }
+}
