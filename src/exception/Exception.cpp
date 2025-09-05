1f917955b226ca154f22e25d40bdf6710a16d63a
Ilya Dyakun <i.dyakun@g.nsu.ru>
2025-09-05 12:12:18 +0700

    Add inheritance from std::exception

--- src/exception/Exception.cpp
+++ src/exception/Exception.cpp
@@ -10,6 +10,11 @@ Exception::Exception(const string& what)
 {}
 
 
+const char* Exception::what() const noexcept {
+  return what_.c_str();
+}
+
+
 ostream& operator<< (ostream& out, const Exception& ex) {
   out << ex.what_ << '\n';
   return out;
--- src/exception/Exception.h
+++ src/exception/Exception.h
@@ -1,19 +1,24 @@
 #ifndef CHIP8_EXCEPTION_EXCEPTION_H
 #define CHIP8_EXCEPTION_EXCEPTION_H
 
+#include <exception>
 #include <ostream>
 #include <string>
 
 namespace chip8 {
 
-class Exception {
+class Exception : public std::exception {
  public:
   Exception(const std::string& what);
+
+  virtual ~Exception() {}
 
   Exception(const Exception&) = default;
   Exception& operator=(const Exception&) = default;
   Exception(Exception&&) = default;
   Exception& operator=(Exception&&) = default;
+
+  virtual const char* what() const noexcept override;
 
   friend std::ostream& operator<< (std::ostream& out, const Exception& ex);
 
